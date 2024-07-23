34440cd8b22d5759c3bdf8a7e7a30c23adfe251d
Junjie Yang <wvfd83@motorolasolutions.com>
2024-07-23 11:50:32 -0400

    added tag for main activity

--- app/src/main/java/com/motorolasolutions/msismartreply/MainActivity.java
+++ app/src/main/java/com/motorolasolutions/msismartreply/MainActivity.java
@@ -1,6 +1,7 @@
 package com.motorolasolutions.msismartreply;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
